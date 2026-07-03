79f4f5f358e3c5172b7298a5d852147a31e61c2b
Gaurav2794 <gaurav274chavan@gmail.com>
2026-07-03 22:24:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.29%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,20 @@
+bool isValid(char *s) {
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            s[++top] = s[i];
+        } else {
+            if (top == -1)
+                return false;
+
+            if ((s[i] == ')' && s[top] != '(') ||
+                (s[i] == '}' && s[top] != '{') ||
+                (s[i] == ']' && s[top] != '['))
+                return false;
+
+            top--;
+        }
+    }
+    return top == -1;
+}
