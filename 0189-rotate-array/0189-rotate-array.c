695c0f90b7d5e675a2d9474be89d9cf61d9f30e3
Gaurav2794 <gaurav274chavan@gmail.com>
2026-07-05 20:16:41 +0530

    Time: 0 ms (100%), Space: 27.2 MB (64.48%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.c
@@ -0,0 +1,10 @@
+void rotate(int* nums, int numsSize, int k) {
+    int temp[numsSize];
+    k = k% numsSize;
+    for(int i=0;i < numsSize ;i++){
+        temp[(i+k) % numsSize]=nums[i];
+    }
+    for(int i = 0; i < numsSize; i++) {
+        nums[i] = temp[i];
+    }
+}
