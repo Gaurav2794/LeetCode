5ed0bbb584b8d466f753c79b304fa67337e26c8d
Gaurav2794 <gaurav274chavan@gmail.com>
2026-07-19 18:23:57 +0530

    Time: 0 ms (100%), Space: 12.2 MB (79.21%) - LeetHub

--- 0112-path-sum/0112-path-sum.c
+++ 0112-path-sum/0112-path-sum.c
@@ -12,6 +12,5 @@ bool hasPathSum(struct TreeNode* root, int targetSum) {
     if (root->left == NULL && root->right == NULL)
         return targetSum == root->val;
 
-    return hasPathSum(root->left, targetSum - root->val) ||
-           hasPathSum(root->right, targetSum - root->val);
+    return hasPathSum(root->left, targetSum - root->val) ||hasPathSum(root->right, targetSum - root->val);
 }
