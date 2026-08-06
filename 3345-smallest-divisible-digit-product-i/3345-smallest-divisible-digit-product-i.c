0f5be78a8cbe4ed5c1a30d2d5b98fcf3159cedfd
Gaurav2794 <gaurav274chavan@gmail.com>
2026-08-06 21:57:03 +0530

    Time: 0 ms (100%), Space: 9.1 MB (16%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.c
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.c
@@ -1,16 +1,16 @@
-int digitproduct(int num){
-    int product = 1;
-    while(num > 0){
-        product = product * (num%10);
-        num = num /10;
-    }
-    return product;
-}
 int smallestNumber(int n, int t) {
-    while(1){
-        if(digitproduct(n)%t == 0)
+    while (1) {
+        int temp = n;
+        int product = 1;
+
+        while (temp > 0) {
+            product *= (temp % 10);
+            temp /= 10;
+        }
+
+        if (product % t == 0)
             return n;
+
         n++;
     }
-    
 }
